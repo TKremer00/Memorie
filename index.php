c946b2b3391df321bee7669bdad1639923dbdf74
Tim Kremer <tim.kremer@unitracer.nl>
2018-10-02 13:21:01 +0200

    Linux ompatible

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ require_once 'memorie.php';
 
 //Gets all images in a folder.
 //Make sure the image for the back of the cards is not in the folder.
-$images = glob('images/*.{jpeg,jpg,gif,png}', GLOB_BRACE);
+$images = glob('images'.DIRECTORY_SEPARATOR.'*.{jpeg,jpg,gif,png}', GLOB_BRACE);
 
 //Place your image for the back of the card as seccond prameter.
-$memorie = new Memorie($images ,'background.jpg');
+$memory = new Memory($images ,'background.jpg');
 
 if(isset($_POST['again']) ) {
   $memorie->restart();
@@ -25,12 +25,12 @@ for ($i=0; $i < $memorie->getSize(); $i++) {
   <head>
     <meta charset="utf-8">
     <title>Memory</title>
-    <link rel="stylesheet" href="Css/style.css">
+    <link rel="stylesheet" href="Css<?php echo DIRECTORY_SEPARATOR; ?>style.css">
 	 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="JS/Javascript.js"> </script>
+    <script src="JS<?php echo DIRECTORY_SEPARATOR; ?>Javascript.js"> </script>
   </head>
   <body>
-    <h2>Memorie <?php echo $memorie->wonTheGame(); ?></h2>
+    <h2>Memory <?php echo $memory->wonTheGame(); ?></h2>
     <?php $memorie->loadField(); ?>
 
   </body>
--- memory.php
+++ memory.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-class Memorie{
+class Memory{
 
   private $size;
   private $turnt ,$turnt_image , $image_id  = [];
